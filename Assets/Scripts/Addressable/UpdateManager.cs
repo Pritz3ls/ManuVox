335b15330814fc080ed1d4a002c56fb754eb0ae3
Pritzels <princefinn14@gmail.com>
2025-03-29 21:03:03 +0800

    Added Packages for CCD Management & Addressables

---
+++ Assets/Scripts/Addressable/UpdateManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class UpdateManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        // Gesture gesture = Addressables.DownloadDependenciesAsync()
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
